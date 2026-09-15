a8f1f0959a7389765dc863e2255d51009d2dfbb6
fkhan313 <farhan.khan615@gmail.com>
2026-09-14 23:55:01 -0400

    style(prettier): format project files

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -2,7 +2,6 @@ import { expect } from '@playwright/test';
 import { test } from '../../fixtures/pages.fixture';
 import { ValidationMessages, AccountMessages } from '../../test-data/messages';
 
-
 test.use({
   storageState: {
     cookies: [],
